0686ef3c427bb14b7a0093fa448f5853351ef41c
Celeste231055 <231055@virtualwindow.co.za>
2023-05-26 12:58:15 +0200

    update

--- WeekThree - Class Files/js/index.js
+++ WeekThree - Class Files/js/index.js
@@ -3,45 +3,43 @@ function oldFunction(data){
     console.log("Hello " + name)
 
 }
-oldFunction(James);
+oldFunction("James");
 
 newFunction = (data) => {
     let name = data;
     console.log("Hello " + name)
 }
 
-newFunction(Timmy);
+newFunction("Timmy");
 
 addStudent = () => {
-    let first = document.getElementById().value
-    let last = document.getElementById().value
-    let age = +document.getElementById().value
-    let subject = document.getElementById().value
+    let first = document.getElementById("first").value;
+    let last = document.getElementById("last").value;
+    let age = +document.getElementById("age").value;
+    let subject = document.getElementById("subject").value;
 
     console.log(first, last, age, subject);
     
     if(age >= 18){
-        alert("Student: " + first + " " + last + "is accepted to " + subject);
+        alert("Student: " + first + " " + last + " is accepted to " + subject);
     } else{
-        alert("Student: " + first + " " + last + "is too young to enroll");
+        alert("Student: " + first + " " + last + " is too young to enroll");
     }
 }
 
 makePizza = () => {
-    let pizzaName = document.getElementById().value
-    let size = document.getElementById().value
+    let pizzaName = document.getElementById("pizzaName").value
+    let size = document.getElementById("size").value
 
     let baseOptions = document.getElementsByName("baseRadio");
-    console.log(baseOptions)
-
     let baseValue;
 
     for(let i = 0; i < baseOptions.length; i++){
         if(baseOptions[i].checked){
-            basevalue = baseOptions[i].value
+            baseValue = baseOptions[i].value
         }
     }
-    console.log(baseValue);
+    
 
     let salami = document.getElementById("salami").checked;
 
